64c9887f4d779d392175db5de55caef080985342
TomDenem1010 <43414435+TomDenem1010@users.noreply.github.com>
2026-09-26 09:36:59 +0200

    #45 chrome path in props

--- src/main/java/trd/home/common/browser/ChromeProcessStarter.java
+++ src/main/java/trd/home/common/browser/ChromeProcessStarter.java
@@ -1,19 +1,28 @@
 package trd.home.common.browser;
 
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import trd.home.common.exception.ChromeLaunchException;
 
 @Component
 public class ChromeProcessStarter {
 
-    private static final String CHROME_EXECUTABLE = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
     private static final String REMOTE_DEBUGGING_PORT = "--remote-debugging-port=9222";
-    private static final String USER_DATA_DIRECTORY = "--user-data-dir=C:\\ChromeProfile";
+
+    private final String executablePath;
+    private final String userDataDirectory;
+
+    public ChromeProcessStarter(
+            @Value("${chrome.executable-path}") String executablePath,
+            @Value("${chrome.user-data-directory}") String userDataDirectory) {
+        this.executablePath = executablePath;
+        this.userDataDirectory = userDataDirectory;
+    }
 
     public void start() {
         try {
-            new ProcessBuilder(CHROME_EXECUTABLE, REMOTE_DEBUGGING_PORT, USER_DATA_DIRECTORY).start();
+            new ProcessBuilder(executablePath, REMOTE_DEBUGGING_PORT, "--user-data-dir=" + userDataDirectory).start();
         } catch (IOException exception) {
             throw new ChromeLaunchException("Unable to create Chrome process", exception);
         }
